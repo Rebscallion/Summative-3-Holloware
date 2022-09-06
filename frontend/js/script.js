80b31a7c901ffaf2448682e48402722026d34aac
Dow-Dodds <100536374+Dow-Dodds@users.noreply.github.com>
2022-09-07 08:39:36 +1200

    added login and register functionality

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,3 @@
-const gridContainer = document.getElementById("grid-container");
 
 
 let showAllProducts = () => {
@@ -17,19 +16,30 @@ let showAllProducts = () => {
 }
 
 let renderProducts = (products) => {
+    const gridContainer = document.getElementById("grid-container");
     console.log("the render projects function is working");
     gridContainer.innerHTML = "";
     products.forEach((item) => {
         gridContainer.innerHTML += `
         <div class="product-wrapper" id="${item._id}">
             <i class="bi bi-heart"></i>
+            <div class="hover-functions">
+               <i class="bi bi-pencil-fill edit-button"></i>
+               <i class="bi bi-trash3-fill delete-button"></i>             
+            </div>
             <img src="${item.image_url}" alt="${item.name}">
             <h3>${item.name}</h3>
             <p>$${item.price}</p>
+            <i class="bi bi-chat-dots"></i>
+
         </div>
         `
     });
 }
 
-
+//-----------------------------
+    // LIST ALL PRODUCTS
+//-----------------------------
 showAllProducts();
+
+
